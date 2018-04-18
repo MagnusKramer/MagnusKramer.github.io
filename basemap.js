6d357f4c62093c0be44c923e92bffa8d25b9df3e
Magnus Kramer <magnus.kramer@student.uibk.ac.at>
2018-04-18 17:35:02 +0200

    basemap erstellen 02

--- basemap.js
+++ basemap.js
@@ -1,8 +1,9 @@
 
 
 let myMap = L.map("mapdiv");
-alert("Hallo")
 let myLayer = L.tileLayer ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+
+let url = "https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{x}/{y}.png"
 
 myMap.addLayer (myLayer);
 myMap.setView([47.267,11.383], 11);
