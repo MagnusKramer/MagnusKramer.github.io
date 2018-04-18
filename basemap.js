65a023bb392217b57b1a07953e471198afa7206a
Magnus Kramer <magnus.kramer@student.uibk.ac.at>
2018-04-18 18:04:26 +0200

    verschiedene Maps dazu

--- basemap.js
+++ basemap.js
@@ -1,10 +1,40 @@
 
 
 let myMap = L.map("mapdiv");
-let myLayer = L.tileLayer ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
 
-let url = "https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{x}/{y}.png"
+let myLayers = {
+    
+    openstreetmap : L.tileLayer (
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    ),
+    geolandbasemap : L.tileLayer (
+        "https://maps1.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"
+    ),
 
-myMap.addLayer (myLayer);
+    bmapoverlay : L.tileLayer (
+        "https://maps1.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"
+    ),
+    
+    bmapgrau : L.tileLayer (
+        "https://maps1.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"
+    ),
+
+    bmaphidpi : L.tileLayer (
+        "https://maps1.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg"
+    ),
+
+    bmaporthofoto30cm : L.tileLayer (
+        "https://maps1.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg"
+    ),
+};
+
+myMap.addLayer (myLayers.bmaporthofoto30cm);
 myMap.setView([47.267,11.383], 11);
 
+/*
+https://maps1.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png
+https://maps1.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png
+https://maps1.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png
+https://maps1.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg
+https://maps1.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg
+*/
