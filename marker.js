5f0472f408c791d1b29694e4f662ee727a016c39
Magnus Kramer <magnus.kramer@student.uibk.ac.at>
2018-04-25 16:53:04 +0200

    brutal

--- marker.js
+++ marker.js
@@ -1,67 +1,74 @@
 
-
-let myMap = L.map("mapdiv");
-
+let myMap = L.map("mapdiv");    // http://leafletjs.com/reference-1.3.0.html#map-l-map
 let myLayers = {
-    
-    openstreetmap : L.tileLayer (
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    osm : L.tileLayer(  // http://leafletjs.com/reference-1.3.0.html#map-l-map
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+          subdomains : ["a", "b","c"],
+          attribution : "Datenquelle: <a href = 'https://www.openstreetmap.org/'> Openstreetmap </a> Mitwirkende"
+        }
     ),
-    geolandbasemap : L.tileLayer (
+    geolandbasemap : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],                          // http://leafletjs.com/reference-1.3.0.html#tilelayer-subdomains
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"    // http://leafletjs.com/reference-1.3.0.html#tilelayer-l-tilelayer
+        }
+    ),
+    bmapoverlay : L.tileLayer(
         "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
             subdomains : ["maps","maps1","maps2","maps3","maps4"],
             attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
         }
     ),
-
-    bmapoverlay : L.tileLayer (
-        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
-            subdomains : ["maps","maps1","maps2","maps3","maps4"],
-            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
-        }
-    ),
-    
-    bmapgrau : L.tileLayer (
+    bmapgrau : L.tileLayer(
         "https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
             subdomains : ["maps","maps1","maps2","maps3","maps4"],
             attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
         }
     ),
-
-    bmaphidpi : L.tileLayer (
+    bmaphidpi : L.tileLayer(
         "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
             subdomains : ["maps","maps1","maps2","maps3","maps4"],
             attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
         }
     ),
-
-    bmaporthofoto30cm : L.tileLayer (
+    bmaporthofoto30cm : L.tileLayer(
         "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
             subdomains : ["maps","maps1","maps2","maps3","maps4"],
             attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
         }
     ),
 };
+myMap.addLayer(myLayers.geolandbasemap);    // http://leafletjs.com/reference-1.3.0.html#layergroup-addlayer
 
-myMap.addLayer (myLayers.bmapgrau);
 
-let myMapControl = L.control.layers({
-    "Open Streetmap" : myLayers.openstreetmap,
-    "Geoland Basemap" : myLayers.geolandbasemap,
-    "Map Grau" : myLayers.bmapgrau,
-    "Map hochaufgeloest" : myLayers.bmaphidpi,
-    "Orthophoto 30cm" : myLayers.bmaporthofoto30cm,
+let myMapControl = L.control.layers({   // http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
+    "Openstreetmap" : myLayers.osm,
+    "basemap.at Grundkarte" : myLayers.geolandbasemap,
+    "basemap.at grau" : myLayers.bmapgrau,
+    "basemap.at highdpi" : myLayers.bmaphidpi,
+    "basemap.at Orthofoto" : myLayers.bmaporthofoto30cm,
 },{
-    "B Map Overlay" : myLayers.bmapoverlay,
-})
+    "basemap.at Overlay" : myLayers.bmapoverlay,
 
-myMap.addControl(myMapControl)
-myMap.setView([47.267,11.383], 11);
+},{
+  collapsed: false,
+});
+let myMapScale = L.control.scale({
+  position: "bottomleft",
+  imperial: false,
+  maxWidth: 200,
 
-/*
-https://maps1.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png
-https://maps1.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png
-https://maps1.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png
-https://maps1.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg
-https://maps1.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg
-*/
+}
+
+).addTo(myMap)
+
+L.marker([47.264,11.385], {
+    title: "Uni Innsbruck",
+    opacity: true;
+}).addTo(myMap);
+
+myMap.addControl(myMapControl); // http://leafletjs.com/reference-1.3.0.html#map-addcontrol
+
+
+myMap.setView([47.267,11.383], 11); // http://leafletjs.com/reference-1.3.0.html#locate-options-setview
+L.marker([47.264,11.385]).addTo(myMap);
