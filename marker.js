9cf4eb0aff5b84cbfddb6bf002ea4aae6626ec8a
Magnus Kramer <magnus.kramer@student.uibk.ac.at>
2018-04-25 17:35:18 +0200

    marker auf karte

--- marker.js
+++ marker.js
@@ -1,5 +1,7 @@
 
 let myMap = L.map("mapdiv");    // http://leafletjs.com/reference-1.3.0.html#map-l-map
+let markerGroup = L.featureGroup();
+
 let myLayers = {
     osm : L.tileLayer(  // http://leafletjs.com/reference-1.3.0.html#map-l-map
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
@@ -39,8 +41,7 @@ let myLayers = {
     ),
 };
 myMap.addLayer(myLayers.geolandbasemap);    // http://leafletjs.com/reference-1.3.0.html#layergroup-addlayer
-
-
+myMap.addLayer()
 let myMapControl = L.control.layers({   // http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
     "Openstreetmap" : myLayers.osm,
     "basemap.at Grundkarte" : myLayers.geolandbasemap,
@@ -49,6 +50,7 @@ let myMapControl = L.control.layers({   // http://leafletjs.com/reference-1.3.0.
     "basemap.at Orthofoto" : myLayers.bmaporthofoto30cm,
 },{
     "basemap.at Overlay" : myLayers.bmapoverlay,
+    "Marker": markerGroup,
 
 },{
   collapsed: false,
@@ -62,16 +64,16 @@ let myMapScale = L.control.scale({
 
 ).addTo(myMap)
 
-L.marker([47.264,11.385], {
+const uni = [47.264, 11.385];
+const usi = [47.257, 11.356];
+const technik = [47.263, 11.343];
+myMap.addLayer(markerGroup);
+const markerOptions = {
     title: "Uni Innsbruck",
-    opacity: 0.4,
-    draggable: true;
-}).addTo(myMap);
-
-myMap.setView([47.264, 11.385], 16);
-
-myMap.addControl(myMapControl); // http://leafletjs.com/reference-1.3.0.html#map-addcontrol
-
-
-myMap.setView([47.267,11.383], 11); // http://leafletjs.com/reference-1.3.0.html#locate-options-setview
-L.marker([47.264,11.385]).addTo(myMap);
+    opacity: 0.7,
+    draggable: true,
+};
+L.marker(uni, markerOptions).addTo(markerGroup)
+L.marker(usi, markerOptions).addTo(markerGroup)
+L.marker(technik, markerOptions).addTo(markerGroup)
+myMap.fitBounds(markerGroup.getBounds())
