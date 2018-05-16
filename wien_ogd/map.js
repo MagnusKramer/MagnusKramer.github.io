3aa79aa23941f75cf1ce8f88a7171c36ada9fd8a
Magnus Kramer <magnus.kramer@student.uibk.ac.at>
2018-05-16 17:01:23 +0200

    icon automatisieren

--- wien_ogd/map.js
+++ wien_ogd/map.js
@@ -65,8 +65,10 @@ L.control.scale({
 }).addTo(karte);
 
 // asynchrone Funktion zum Laden eines GeoJSON Layers
-async function ladeGeojsonLayer(url) {
-    const response = await fetch(url);
+async function ladeGeojsonLayer(datenAttribute) {
+    console.log(datenAttribute);
+    console.log(datenAttribute.icon);
+    const response = await fetch(datenAttribute.json);
     const response_json = await response.json();
 
     // GeoJSON Geometrien hinzufügen und auf Ausschnitt zoomen
@@ -104,15 +106,18 @@ wienDatensaetze.sort(function(a,b) {
 })
 
 // den GeoJSON Layer für den ersten Datensatz laden
-ladeGeojsonLayer(wienDatensaetze[0].json);
+ladeGeojsonLayer(wienDatensaetze[0]);
 
 let layerAuswahl = document.getElementById("layerAuswahl");
-for (datensatz of wienDatensaetze) {
-    layerAuswahl.innerHTML += `<option value="${datensatz.json}">${datensatz.titel}</option>`
-    //console.log(datensatz.json)
+for (let i=0; i<wienDatensaetze.length; i++) {
+    layerAuswahl.innerHTML += `<option value="${i}">${wienDatensaetze[i].titel}</option>`
+    console.log(i, wienDatensaetze[i].titel)
+    
 }
 
 layerAuswahl.onchange = function(evt) {
     geojsonGruppe.clearLayers();
-    ladeGeojsonLayer(evt.target.value);
+    let i = evt.target.value;
+   // console.log(i, wienDatensaetze[i])
+    ladeGeojsonLayer(wienDatensaetze[i]);
 }
